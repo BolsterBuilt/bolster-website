64210067872c33a12756d422d140a059cd724e04
Corbin.B <corbin.boonzaier@googlemail.com>
2024-04-12 00:02:24 +0200

    files

---
+++ next.config.mjs
@@ -0,0 +1,16 @@
+import BuilderDevTools from "@builder.io/dev-tools/next";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = BuilderDevTools()(
+  BuilderDevTools()(
+    BuilderDevTools()(
+      BuilderDevTools()(
+        BuilderDevTools()(
+          BuilderDevTools()(BuilderDevTools()(BuilderDevTools()({})))
+        )
+      )
+    )
+  )
+);
+
+export default nextConfig;
