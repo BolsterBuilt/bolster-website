ea42259b376d6c9939b2388bb22d7f1cc4add4ac
Corbin.B <corbin.boonzaier@googlemail.com>
2024-04-19 12:52:41 +0200

    hme

--- next.config.js
+++ next.config.js
@@ -2,29 +2,26 @@ const withBuilderDevTools = require('@builder.io/dev-tools/next')()
 const bundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: !!process.env.BUNDLE_ANALYZE,
 })
-module.exports = {
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/home',
-        permanent: true,
-      },
-    ]
-  },
-}
 
 module.exports = withBuilderDevTools(
   bundleAnalyzer({
     images: {
       domains: ['cdn.builder.io'],
     },
+    async redirects() {
+      return [
+        {
+          source: '/',
+          destination: '/home',
+          permanent: true,
+        },
+      ]
+    },
     async headers() {
       return [
         {
           source: '/:path*',
           headers: [
-            // this will allow site to be framed under builder.io for wysiwyg editing
             {
               key: 'Content-Security-Policy',
               value: 'frame-ancestors https://*.builder.io https://builder.io',
@@ -34,7 +31,6 @@ module.exports = withBuilderDevTools(
       ]
     },
     env: {
-      // expose env to the browser
       BUILDER_PUBLIC_KEY: process.env.BUILDER_PUBLIC_KEY,
     },
   })
